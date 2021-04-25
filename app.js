19a29ea5671d49e863b0442938906edebb26d9da
Nayan CM <nayan15-10316@diu.edu.bd>
2021-04-26 04:00:46 +0600

    World toure final done

--- app.js
+++ app.js
@@ -12,8 +12,27 @@ const displayCountries = countries => {
         const nameAndCapitalInfo = `
         <h2 id="countryId">Country: ${country.name}</h2>
         <h3 id="capitalId">Capital: ${country.capital}</h3>
+        <button id="countryDetail" onclick="showMoreInfo('${country.name}')">Details...</button>
         `
         nameAndCapitalDiv.innerHTML = nameAndCapitalInfo;
         countryInfo.appendChild(nameAndCapitalDiv);
+
     });
+}
+
+const showMoreInfo = countryName =>{
+    const url = `https://restcountries.eu/rest/v2/name/${countryName}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderInfo(data[0]));
+}
+
+const renderInfo = oneCountry =>{
+    const countryMoreInfos = document.getElementById('countryMoreInfo');
+    countryMoreInfos.innerHTML = `
+    <h1>Name: ${oneCountry.name}</h1>
+    <h3>Area: ${oneCountry.area}</h3>
+    <h3>Population: ${oneCountry.population}</h3>
+    <img src="${oneCountry.flag}">
+    `
 }
